d142f5b32726f5aebb20f1bb37d465bd24b72bb4
Ibitsam285 <30135822@students.southwales.ac.uk>
2025-03-19 01:45:53 +0500

    Auth functionality complete. Now only users with valid token can access the API

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 // Import route files
+const authRoutes = require("./routes/auth");
 const movieRoutes = require("./routes/movies");
 const directorRoutes = require("./routes/directors");
 const genreRoutes = require("./routes/genres");
@@ -24,10 +25,14 @@ mongoose
   .catch((err) => console.error("MongoDB connection error:", err));
 
 // Routes
+app.use("/api/auth", authRoutes);
 app.use("/api/movies", movieRoutes);
 app.use("/api/directors", directorRoutes);
 app.use("/api/genres", genreRoutes);
 app.use("/api/actors", actorRoutes);
 app.use("/api/movieActors", movieActorRoutes);
 
-module.exports = app;  // Export app for testing
+// app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+
+// For Testing, stopping .listen
+module.exports = app
